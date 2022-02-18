7f74ff9c542c71951320c1ae4bea73d51965d140
Eliabe-Rocha <eliabe-rocha@hotmail.com>
2022-02-17 23:51:21 -0300

    Commit de atualização de material básico em JS

--- Extra - Basico em JavaScript/aula 1/sintaxe.js
+++ Extra - Basico em JavaScript/aula 1/sintaxe.js
@@ -1,1 +1,29 @@
-alert ("JavaScript Implantado")
+//alert ("JavaScript Implantado")
+
+/*var x = 5;
+var y = 10;
+var z = "15";
+var c = "5";
+var a = x < 6;
+var b = y >= 10;
+
+console.log(x, typeof(x))
+console.log(y, typeof(y))
+console.log(z, typeof(z))
+
+console.log(a && b);
+console.log(a || b);
+console.log(!b && a);
+console.log(x == c);
+console.log(x === z);
+
+let box = {numero: 10, testo: 'Teste', logico: true, matrix: ['matrix'], caixa: 'Objeto_Interno'};
+
+console.log(box);
+
+var primeiro = box.numero;
+console.log(primeiro);*/
+
+
+
+
